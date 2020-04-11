4c1f51487139bd1d44340739f60611f155ebafc7
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-11 17:50:21 +0700

    #14 - Implement routing progress bar indicator

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
+import NProgress from 'nprogress';
 
 import App from './App.vue'
 import Create from './components/Create.vue';
@@ -9,6 +10,7 @@ import Edit from './components/Edit.vue';
 import Index from './components/Index.vue';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/nprogress/nprogress.css';
 
 Vue.use(VueRouter);
 Vue.use(VueAxios, axios);
@@ -34,6 +36,17 @@ const routes = [
 ];
 
 const router = new VueRouter({mode: 'history', routes: routes});
+
+router.beforeResolve((to, from, next) => {
+  if (to.name) {
+    NProgress.start()
+  }
+  next()
+});
+
+router.afterEach(() => {
+  NProgress.done()
+});
 
 new Vue({
   render: h => h(App),
