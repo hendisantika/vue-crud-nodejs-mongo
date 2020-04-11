ea21d7e9a559884f2ae60d36aaa55eed25834a86
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-11 17:44:34 +0700

    #13 - Add Bootstrap 4 Navigation

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,28 @@
     </transition>
 </template>
 
+<template>
+    <div class="container" id="app">
+        <nav class="navbar navbar-expand-sm bg-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <router-link :to="{ name: 'Create' }" class="nav-link">Add Item</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link :to="{ name: 'Index' }" class="nav-link">All Items</router-link>
+                </li>
+            </ul>
+        </nav>
+        <transition name="fade">
+            <div class="gap">
+                <router-view></router-view>
+            </div>
+        </transition>
+    </div>
+</template>
+
 
 <script>
-
     export default {}
 </script>
 
@@ -19,5 +38,9 @@
 
     .fade-enter, .fade-leave-active {
         opacity: 0
+    }
+
+    .gap {
+        margin-top: 50px;
     }
 </style>
