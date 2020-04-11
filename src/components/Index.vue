f41ea89e1249696faf25f6deee4a11a36dddf510
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-11 18:23:11 +0700

    #26 - Display the items

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -8,3 +8,61 @@
 <script>
     export default {}
 </script>
+
+<template>
+    <div>
+        <h1>Items</h1>
+
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <td>ID</td>
+                <td>Item Name</td>
+                <td>Item Price</td>
+                <td>Actions</td>
+            </tr>
+            </thead>
+
+            <tbody>
+            <tr :key="item._id" v-for="item in items">
+                <td>{{ item._id }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.price }}</td>
+                <td>
+                    <router-link :to="{name: 'Edit', params: { id: item._id }}" class="btn btn-primary">Edit
+                    </router-link>
+                </td>
+                <td>
+                    <button class="btn btn-danger">Delete</button>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    data()
+    {
+        return {
+            items: []
+        }
+    }
+    ,
+
+    created: function () {
+        this.fetchItems();
+    }
+    ,
+
+    {
+        fetchItems()
+        {
+            let uri = 'http://localhost:4000/items';
+            this.axios.get(uri).then((response) => {
+                this.items = response.data;
+            });
+        }
+    }
+    }
+</script>
