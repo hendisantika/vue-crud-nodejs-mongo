9552dbc6cf654e51f9174985c6cd4d77f862d7f0
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-11 17:51:55 +0700

    #15 - Create a form inside create.vue component

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,10 +1,34 @@
 // Create.vue
 
 <template>
-    <div>
-        Create Component
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h3>Add Item</h3>
+            </div>
+            <div class="card-body">
+                <form v-on:submit.prevent="addItem">
+                    <div class="form-group">
+                        <label>Item Name:</label>
+                        <input class="form-control" type="text"/>
+                    </div>
+                    <div class="form-group">
+                        <label>Item Price:</label>
+                        <input class="form-control" type="text"/>
+                    </div>
+                    <div class="form-group">
+                        <input class="btn btn-primary" type="submit" value="Add Item"/>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
+
 <script>
-    export default {}
+    export default {
+        components: {
+            name: 'AddItem'
+        }
+    }
 </script>
